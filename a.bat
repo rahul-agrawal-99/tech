8e4efcfe8344f82994692d065b068afc6f3bd3ef
rahul99 <agrawal.rahul20022@gmail.com>
2022-01-31 10:47:31 +0530

    eg

--- a.bat
+++ a.bat
@@ -1,10 +1,15 @@
+@ECHO OFF
+set /p pull= "Enter 0 to leave pull otherwise any btn will pull first : " 
+
+if %pull%==0 (echo "pulling") else (echo "skip pull") 
+
 set /p choice= "Enter Message to be set on Commit : " 
 git add .
 git commit -m "%choice%"
 git push origin main 
 git log --oneline
 git log -1 --stat
-@ECHO OFF
+
 ECHO files commited and pushed successfully to github main branch
 ECHO Press Any key to exit
 PAUSE
